a2f4d76b98daab6eed5f555d90a795db4e5292ea
homeschooled <35203913+homeschooled@users.noreply.github.com>
2020-09-02 18:32:57 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,35 @@
-var ytSpeed;void 0===ytSpeed&&(ytSpeed={playbackRate:1,preservesPitch:!1,init:function(){new MutationObserver(function(a){ytSpeed.updateVideos()}).observe(document.querySelector("body"),{attributes:!0,childList:!0,characterData:!0,subtree:!0}),ytSpeed.updateVideos()},updateVideos:function(){for(var a=document.getElementsByTagName("video"),b=0;b<a.length;++b){var c=a[b];c.playbackRate=this.playbackRate,c.mozPreservesPitch=this.preservesPitch&&1!=this.playbackRate}},speedUp:function(){this.playbackRate*=1.05946309436,ytSpeed.updateVideos()},speedDown:function(){this.playbackRate/=1.05946309436,ytSpeed.updateVideos()},reset:function(){this.playbackRate=1,ytSpeed.updateVideos()},prompt:function(){var a=prompt("New playback speed:",this.playbackRate);a&&(this.playbackRate=a,ytSpeed.updateVideos())}},ytSpeed.init()); 
+var ytPitch;
+void 0 === ytPitch && (ytPitch = {
+    playbackRate: 1,
+    preservesPitch: !1,
+    init: function() {
+        new MutationObserver(function(a) {
+            ytPitch.updateVideos()
+        }).observe(document.querySelector("body"), {
+            attributes: !0,
+            childList: !0,
+            characterData: !0,
+            subtree: !0
+        }), ytPitch.updateVideos()
+    },
+    updateVideos: function() {
+        for (var a = document.getElementsByTagName("video"), b = 0; b < a.length; ++b) {
+            var c = a[b];
+            c.playbackRate = this.playbackRate, c.mozPreservesPitch = this.preservesPitch && 1 != this.playbackRate
+        }
+    },
+    PitchUp: function() {
+        this.playbackRate *= 1.05, ytPitch.updateVideos()
+    },
+    PitchDown: function() {
+        this.playbackRate /= 1.05, ytPitch.updateVideos()
+    },
+    reset: function() {
+        this.playbackRate = 1, ytPitch.updateVideos()
+    },
+    prompt: function() {
+        var a = prompt("New playback speed:", this.playbackRate);
+        a && (this.playbackRate = a, ytPitch.updateVideos())
+    }
+}, ytPitch.init());
 
-console.log("main Pitchify loaded!");
-console.log("Pitchify loaded?!");
-
-document.querySelector('button').addEventListener('click', () =>{
-	console.log('u pressed me tbh.')
-})
