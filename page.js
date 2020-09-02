b49d1426411a8d615cbff5853578f75844e70d30
homeschooled <35203913+homeschooled@users.noreply.github.com>
2020-09-02 18:34:08 +0200

    Update page.js

--- page.js
+++ page.js
@@ -1,11 +1,7 @@
 console.log("Pitchify loaded!");
-// var ytSpeed;void 0===ytSpeed&&(ytSpeed={playbackRate:1,preservesPitch:!1,init:function(){new MutationObserver(function(a){ytSpeed.updateVideos()}).observe(document.querySelector("body"),{attributes:!0,childList:!0,characterData:!0,subtree:!0}),ytSpeed.updateVideos()},updateVideos:function(){for(var a=document.getElementsByTagName("video"),b=0;b<a.length;++b){var c=a[b];c.playbackRate=this.playbackRate,c.mozPreservesPitch=this.preservesPitch&&1!=this.playbackRate}},speedUp:function(){this.playbackRate*=1.05946309436,ytSpeed.updateVideos()},speedDown:function(){this.playbackRate/=1.05946309436,ytSpeed.updateVideos()},reset:function(){this.playbackRate=1,ytSpeed.updateVideos()},prompt:function(){var a=prompt("New playback speed:",this.playbackRate);a&&(this.playbackRate=a,ytSpeed.updateVideos())}},ytSpeed.init()); 
+ytPitch.prompt();
 
-ytSpeed.prompt();
+//still//
 
-
-// document.querySelector('button').addEventListener('click', () =>{
-// 	console.log('u pressed me tbh.')
-// })
 
 
